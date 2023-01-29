6f135844d267008a948b32153458ef2e308b59ec
Sumanth3028 <sumanthakula05@gmail.com>
2023-01-29 01:03:56 -0800

    css

--- product.js
+++ product.js
@@ -13,7 +13,7 @@ function onAddProduct(e){
     obj={sellingprice,addProduct}
     objects.push(obj);
     //pushing elements into crud
-    axios.post("https://crudcrud.com/api/4b881f5cbe114018abff7405e00f042a/productDetails",obj)
+    axios.post("https://crudcrud.com/api/7c574feee53f453a9c5460d4ba44137c/productDetails",obj)
     .then((res)=>{
         console.log(res)
     calculate(res.data.sellingprice)}).catch(err=>console.error(err));
@@ -21,7 +21,7 @@ function onAddProduct(e){
     
 }
 window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/4b881f5cbe114018abff7405e00f042a/productDetails")
+    axios.get("https://crudcrud.com/api/7c574feee53f453a9c5460d4ba44137c/productDetails")
     .then(res=>{
         console.log(res);
         for(var i=0;i<res.data.length;i++){
@@ -33,13 +33,14 @@ window.addEventListener("DOMContentLoaded",()=>{
 });
 function showUserOnScreen(objects){
   var li=document.createElement('li');
-  li.textContent=`${objects.sellingprice}-${objects.addProduct}`;
+  li.textContent=`${objects.sellingprice}-${objects.addProduct}    `;
   var delbtn=document.createElement('input');
   delbtn.type='button';
   delbtn.value='Delete Product';
+  delbtn.style='background-color:red';
   delbtn.onclick=()=>{
     itemList.removeChild(li);
-    axios.delete(`https://crudcrud.com/api/4b881f5cbe114018abff7405e00f042a/productDetails/${objects._id}`)
+    axios.delete(`https://crudcrud.com/api/7c574feee53f453a9c5460d4ba44137c/productDetails/${objects._id}`)
     .then(res=>{console.log(res) 
         subtract(objects.sellingprice)}).catch(err=>console.error(err))
           
